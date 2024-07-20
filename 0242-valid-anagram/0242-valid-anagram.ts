f67d5113cb0ce468e8115029132a06d5c5fd5383
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2024-07-20 21:52:32 +0530

    Time: 71 ms (72.14%), Space: 57.5 MB (5.23%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -0,0 +1,28 @@
+function isAnagram(s: string, t: string): boolean {
+    s = s.toLowerCase();
+    t = t.toLowerCase();
+    if(s.length !== t.length){
+        return false;
+    }
+    let fc1: {[key:string]:number} = {};
+    let fc2: {[key:string]:number} = {};
+
+    for(let val of s){
+        fc1[val] = (fc1[val]|| 0)+1;
+    }
+
+    for(let val of t){
+        fc2[val] = (fc2[val]||0)+1;
+    }
+    console.log(fc1, fc2);
+
+    for(let key in fc1){
+        if(!(key in fc2)){
+            return false;
+        }
+        if(fc1[key]!==fc2[key]){
+            return false;
+        }
+    }
+    return true;
+};
