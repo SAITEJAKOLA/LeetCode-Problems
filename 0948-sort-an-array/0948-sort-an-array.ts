5109aff4acd2bad04c3bf62458b2749740531078
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2024-07-26 14:19:18 +0530

    Time: 220 ms (64.27%), Space: 74.6 MB (34.7%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.ts
+++ 0948-sort-an-array/0948-sort-an-array.ts
@@ -1,7 +1,27 @@
 function sortArray(arr: number[]): number[] {
     if(arr.length<=1) return arr;
     function merge(arr1: number[], arr2: number[]): number[] {
-      return arr1.concat(arr2).sort((a, b) => a - b);
+        let result:number[] = [];
+        let i=0;
+        let j=0;
+        while(i<arr1.length && j<arr2.length){
+            if(arr2[j]>=arr1[i]){
+                result.push(arr1[i]);
+                i++;
+            }else{
+                result.push(arr2[j]);
+                j++
+            }
+        }
+        while(i<arr1.length){
+            result.push(arr1[i]);
+            i++;
+        }
+        while(j<arr2.length){
+            result.push(arr2[j]);
+            j++;
+        }
+        return result;
     }
     let mid = Math.floor(arr.length/2);
     let left = sortArray(arr.slice(0,mid));
