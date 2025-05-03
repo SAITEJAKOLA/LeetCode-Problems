cebe9eaf27ed0e3d925a9a5373337dbaad5b7c81
saitejakola <saiteja.01999@gmail.com>
2025-05-03 17:07:00 +0530

    Refractor

--- BigO/frequencyCounter.ts
+++ BigO/frequencyCounter.ts
@@ -1,20 +1,31 @@
 //Write a function which takes in a string and returns counts of each character in the string.
 
-function charCount(str: string): { [key: string]: number } {
+import { realpathSync } from 'fs';
 
+function charCount(str: string): { [key: string]: number } {
     if (str.length === 0) return {}; //defualt case
     // fc is frequency counter
     let fc: { [key: string]: number } = {};
 
-    //Trim the string and remove spaces
-    str = str.trim().split(' ').join('');
     for (let val of str) {
         val = val.toLowerCase(); //convert to lower case
-        fc[val] = (fc[val] || 0) + 1;
-    }
-    console.log(fc)
-    return fc;
 
+        //one way fo doing is using regX /[a-z0-9]/.test(val)
+        if (isAlphraNumeric(val)) fc[val] = (fc[val] || 0) + 1;
+    }
+    console.log(fc);
+    return fc;
+}
+function isAlphraNumeric(char: string): boolean {
+    let code = char.charCodeAt(0);
+    if (
+        !(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) &&
+        !(code > 96 && code < 123)
+    ) {
+        return false;
+    }
+    return true;
 }
 
-charCount('Hello Teja 1234')
+charCount('Hello Teja 1234!!!');
