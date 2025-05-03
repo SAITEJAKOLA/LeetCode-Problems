31a63662111e6abefd9d30defdba0b5fabffdca0
saitejakola <saiteja.01999@gmail.com>
2025-05-03 16:13:23 +0530

    Added corner cases for fc

--- BigO/frequencyCounter.ts
+++ BigO/frequencyCounter.ts
@@ -3,15 +3,18 @@
 function charCount(str: string): { [key: string]: number } {
 
     if (str.length === 0) return {}; //defualt case
-
+    // fc is frequency counter
     let fc: { [key: string]: number } = {};
 
+    //Trim the string and remove spaces
+    str = str.trim().split(' ').join('');
     for (let val of str) {
+        val = val.toLowerCase(); //convert to lower case
         fc[val] = (fc[val] || 0) + 1;
     }
     console.log(fc)
     return fc;
-    
+
 }
 
-charCount('HelloTeja1234')
+charCount('Hello Teja 1234')
