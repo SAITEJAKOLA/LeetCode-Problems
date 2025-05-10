f758f804cf2f5e193802c90e4f06a784bdc0be16
saitejakola <saiteja.01999@gmail.com>
2025-05-10 12:44:20 +0530

    multipointer practice

---
+++ Problem-Solving-Patterns/MultiplePointerspattern.ts
@@ -0,0 +1,74 @@
+/*
+Multiple pointers 
+Creating pointers or values that correspond to an idex or position
+and move towards the begining, end or middle based on certain condition.
+
+Very efficient for solving problems with minimal space complecity as well.
+losely defined, the direction of pointers deepending upon the problem.
+*/
+
+//SumZero - which accepts a sorted array of integers. The function should find the first pair where the sum is 0.
+//Return an array that includes both the values that sum zero or undefined if a pair does not exist.
+//SumZero([-3,-2,-1,0,1,2,3])retuns [-3,3]
+
+function sumZero(arr: []): number[] {
+    if (arr.length === 0) return []
+    for (let i = 0; i <= arr.length - 1; i++) {
+        for (let j = i + 1; j <= arr.length - 1; j++) {
+            if (arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]]
+            }
+        }
+    }
+    return []
+}
+
+//using 2 pointer mechanisam
+
+function sumZeroM(arr: number[]): number[] {
+    if (arr.length === 0) return []
+    let left = 0
+    let right = arr.length - 1
+    while (left < right) {
+        let sum = arr[left] + arr[right]
+        if (sum === 0) return [arr[left], arr[right]]
+        if (sum > 0) right--
+        else left++
+    }
+    return []
+}
+console.log(sumZeroM([-4, -3, -2, -1, 0, 1, 2, 3, 4, 5]));
+
+
+
+//CountUniqueValues
+//this can be dont using frequency counters and multiple pointers also
+//The function takes an sorted array, there can be both + and -ve values.
+
+//using multiple pointers.
+
+function CountUniqueValues(arr: number[]): number {
+    if (arr.length === 0) return 0
+    let i = 0;
+    for (let j = 1; j <= arr.length - 1; j++) {
+        if (arr[i] === arr[j]) j++;
+        if (arr[i] != arr[j]) {
+            i++;
+            arr[i] = arr[j];
+            j++;
+        }
+    }
+    return i + 1;
+}
+console.log(CountUniqueValues([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]));
+
+
+//Easier way of doing is using frequency counters
+function CountUniqueValuesUsingFC(arr: number[]): number {
+    if (arr.length === 0) return 0
+    let fc: { [key: number]: number } = {}
+    for (let val of arr) fc[val] = (fc[val] || 0) + 1
+    return Object.keys(fc).length
+}
+
+console.log(CountUniqueValuesUsingFC([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]));
---
+++ Problem-Solving-Patterns/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+//CountUniqueValues
+//this can be dont using frequency counters and multiple pointers also
+//The function takes an sorted array, there can be both + and -ve values.
+
+//using multiple pointers.
