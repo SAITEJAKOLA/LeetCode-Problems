0850dd2b64343384efd9c4ff8896ae76eab576f6
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2025-09-16 23:11:15 +0530

    Time: 38 ms (31.31%), Space: 70.9 MB (8.65%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -1,36 +1,13 @@
 function groupAnagrams(strs: string[]): string[][] {
-  if (strs.length === 0) return [];
+    if(strs.length===0) return [];
+    let map: {[key:string]:string[]}={};
 
-  // Function to sort a string
-  function sortString(str: string): string {
-    return str.split('').sort().join('');
-  }
-
-  // Sort the strings based on their sorted version
-  const sortedStrs = strs.map(str => ({ original: str, sorted: sortString(str) }));
-  sortedStrs.sort((a, b) => a.sorted.localeCompare(b.sorted));
-
-  // Group the anagrams
-  const output: string[][] = [];
-  let currentGroup: string[] = [];
-  let currentKey = sortedStrs[0].sorted;
-
-  for (let i = 0; i < sortedStrs.length; i++) {
-    const { original, sorted } = sortedStrs[i];
-
-    if (sorted === currentKey) {
-      currentGroup.push(original);
-    } else {
-      output.push(currentGroup);
-      currentGroup = [original];
-      currentKey = sorted;
+    for(let str of strs){
+        let sortedString = str.split('').sort().join('');
+        if(!map[sortedString]){
+            map[sortedString] = [];
+        }
+        map[sortedString].push(str)
     }
-  }
-
-  // Push the last group
-  if (currentGroup.length > 0) {
-    output.push(currentGroup);
-  }
-
-  return output;
-}
+    return Object.values(map);
+};
