2f4ac36ec6be49f02cf2ee68ab2de45ea10a79f4
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2025-09-17 22:22:30 +0530

    Time: 9 ms (78.5%), Space: 59.4 MB (84.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
@@ -1,11 +1,12 @@
 function topKFrequent(nums: number[], k: number): number[] {
-    if(nums.length ===0 ) return nums;
-    let fc: { [key: number]: number } = {};
-
+    if(nums.length===0) return [];
+    let map: {[key:number]:number}={};
     for(let val of nums){
-        fc[val] = (fc[val]||0)+1;
+        map[val]= (map[val]|0)+1;
     }
-    const sortedArray = Object.entries(fc).sort((a,b)=> b[1]-a[1]);
-    const finalArray = sortedArray.slice(0,k).map((val)=> parseInt(val[0]));
-    return finalArray;
+    //Object.entries will return something like this [[key, value], [key, value]]
+	//sort the all the entries [key, value]
+    const sortedArray = Object.entries(map).sort((a,b)=>b[1]-a[1]);
+    //sortedArrya.slice(0,k) slices from 0 to k and top of that map return a [].
+    return sortedArray.slice(0,k).map((val)=> parseInt(val[0]));
 };
