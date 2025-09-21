e0442a3bb2ac96adf15349ba8db03fa767d2d69d
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2025-09-21 23:13:22 +0530

    Time: 108 ms (6.39%), Space: 70 MB (20.6%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -1,13 +1,12 @@
 function groupAnagrams(strs: string[]): string[][] {
-    if(strs.length===0) return [];
-    let map: {[key:string]:string[]}={};
+	if (strs.length === 0) return [];
+	let map = new Map<string, string[]>();
 
-    for(let str of strs){
-        let sortedString = str.split('').sort().join('');
-        if(!map[sortedString]){
-            map[sortedString] = [];
-        }
-        map[sortedString].push(str)
-    }
-    return Object.values(map);
+	for (let value of strs) {
+		let sortedString = value.split('').sort().join('');
+		console.log(sortedString);
+		if (!map.has(sortedString)) map.set(sortedString, []);
+		map.get(sortedString)!.push(value);
+	}
+	return Array.from(map.values());
 };
