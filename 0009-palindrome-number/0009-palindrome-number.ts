434a5d73917735444a0bb3b5354280438cd930b9
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2025-10-25 11:25:02 +0530

    Time: 8 ms (49.27%), Space: 65.3 MB (52.74%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,8 +1,14 @@
 function isPalindrome(n: number): boolean {
 	// Handling negative numbers
-	if (n === 0) return true;
-	let nCopy = n;
-    let str = n.toString().split("").reverse().join("");
-    if(nCopy == parseInt(str)) return true
-    else return false
+	if (n < 0) return false;
+	let str = n.toString();
+	let left = 0;
+	let right = str.length - 1;
+	let middle = Math.floor(str.length / 2);
+	while (left <= middle && right >= middle) {
+		if (str[left] !== str[right]) return false;
+		left++;
+		right--;
+	}
+	return true;
 }
