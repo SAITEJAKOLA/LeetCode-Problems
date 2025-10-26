e6c9b90681220354c50be13335205bb6dd59bdb4
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2025-10-26 12:53:04 +0530

    Time: 0 ms (100%), Space: 59.3 MB (19.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,18 +1,11 @@
 function removeDuplicates(nums: number[]): number {
-    if(nums.length===0) return 0;
-    let i=0;
-    let j=i+1;
-    while(j<nums.length){
-        //Now the idea is to use the same array to manitain the unique element count.
-        //By copying the non repated numbers to the front of array.
-        if(nums[i]===nums[j]) j++;
-        else if(nums[i]!==nums[j]){
-            i++;
-            nums[i]= nums[j];
-            j++;
+    if(nums.length === 0) return 0;
+    let x = 0;
+    for(let i = 0; i< nums.length; i++){
+        if(nums[i]>nums[x]){
+            x++;
+            nums[x] = nums[i]
         }
     }
-    //As asked in the question we print the nums also by replacing non unique values will some other value.
-    console.log(nums.fill(0,i+1,nums.length))
-    return i+1;
+    return x+1;
 };
