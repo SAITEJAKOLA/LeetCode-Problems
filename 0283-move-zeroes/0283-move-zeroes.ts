ca4599b4e81c2681c7e70d0f7742c6c22d19f77e
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2025-11-01 12:25:06 +0530

    Time: 9 ms (18.25%), Space: 61.3 MB (34.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.ts
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -2,18 +2,18 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-	let i = 0;
-	let j = i + 1;
-	while (j <= nums.length - 1) {
-		if (nums[i] === 0 && nums[i] !== nums[j]) {
-			[nums[i], nums[j]] = [nums[j], nums[i]];
-			i++;
-			j++;
-		} else if (nums[i] === 0 && nums[j] === 0) j++;
-		else {
-			i++;
-			j++;
-		}
-	}
-	console.log(nums);
-}
+    let i= 0;
+    let j =1;
+    while(j<nums.length){
+        if(nums[i]===0 && nums[i]!==nums[j]){
+            [nums[i],nums[j]]=[nums[j],nums[i]]
+            i++;
+            j++;
+        } else if(nums[i]===0 && nums[j]===0){
+            j++
+        } else{
+            i++;
+            j++
+        }
+    }
+};
