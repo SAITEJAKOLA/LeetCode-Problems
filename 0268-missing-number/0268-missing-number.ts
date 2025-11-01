6f228dfd87713ae5898e3c6b31d548c72be748c1
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2025-11-01 15:17:59 +0530

    Time: 1 ms (75.79%), Space: 57.8 MB (74.63%) - LeetHub

--- 0268-missing-number/0268-missing-number.ts
+++ 0268-missing-number/0268-missing-number.ts
@@ -1,11 +1,12 @@
 function missingNumber(nums: number[]): number {
-    //first loop through the array and find the sum of all elements and also find the max in the array.
+    //first loop through the array and find the sum of all elements.
+    //n = nums.length.
     let sum = 0;
-    let max = nums.length;
+    let n = nums.length;
     for(let i = 0; i< nums.length; i++){
         sum = sum + nums[i];
     }
-    let expectedSum = max*(max+1)/2;
+    let expectedSum = n*(n+1)/2;
     if(sum === expectedSum) return 0;
     return expectedSum - sum;
 };
