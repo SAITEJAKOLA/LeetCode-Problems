675f76948f4ad865e4e10befee4100068de79e21
SAITEJAKOLA <85824348+SAITEJAKOLA@users.noreply.github.com>
2025-12-28 00:10:41 +0530

    Time: 39 ms (87.15%), Space: 55.7 MB (10.42%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.ts
+++ 0509-fibonacci-number/0509-fibonacci-number.ts
@@ -1,6 +1,11 @@
 function fib(n: number): number {
   // Iterative approach
   let arr = new Array(n);
+  /*
+  f(0) = 0;
+  f(1) = 1;
+  f(n) = f(n-1)+f(n-2)
+  */
   arr[0] = 0;
   arr[1] = 1;
   for(let i=2; i<=n; i++){
