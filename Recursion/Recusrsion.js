3f7ccf4e8a652848ca452d6f00757ee49fcce11a
SAITEJAKOLA <saitejakola@outlook.com>
2025-12-29 22:19:57 +0530

    P 29th Dec 2025

--- Recursion/Recusrsion.js
+++ Recursion/Recusrsion.js
@@ -25,30 +25,30 @@ Where things can go wrong.
 */
 
 function countDown(num) {
-  if (num <= 0) {
-    //Base case
-    console.log('All done!');
-    return;
-  }
-  console.log(num);
-  num--; //different value
-  countDown(num);
+	if (num <= 0) {
+		//Base case
+		console.log('All done!');
+		return;
+	}
+	console.log(num);
+	num--; //different value
+	countDown(num);
 }
 
 countDown(5);
 
 function sumRange(num) {
-  if (num === 1) return 1;
-  return num + sumRange(num - 1);
+	if (num === 1) return 1;
+	return num + sumRange(num - 1);
 }
 sumRange(4);
 
 //factorial
 function factorial(num) {
-  if (num === 0) {
-    return 1;
-  }
-  return num * factorial(num - 1);
+	if (num === 0) {
+		return 1;
+	}
+	return num * factorial(num - 1);
 }
 factorial(5);
 
@@ -70,16 +70,16 @@ function outer(input){
 //collecting all odd values of an array
 
 function collectOddValues(arr) {
-  let result = [];
-  function helper(helperInput) {
-    if (helperInput.length === 0) return;
-    if (helperInput[0] % 2 !== 0) {
-      result.push(helperInput[0]);
-    }
-    helper(helperInput.slice(1));
-  }
-  helper(arr);
-  return result;
+	let result = [];
+	function helper(helperInput) {
+		if (helperInput.length === 0) return;
+		if (helperInput[0] % 2 !== 0) {
+			result.push(helperInput[0]);
+		}
+		helper(helperInput.slice(1));
+	}
+	helper(arr);
+	return result;
 }
 
 collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
@@ -92,13 +92,13 @@ pure recursion
 */
 
 function collectOddValuesR(arr) {
-  let arrnew = [];
-  if (arr.length === 0) return;
-  if (arr[0] % 2 !== 0) {
-    arrnew.push(arr[0]);
-  }
-  arrnew = arrnew.concat(collectOddValuesR(arr.slice(1)));
-  return arrnew;
+	let arrnew = [];
+	if (arr.length === 0) return;
+	if (arr[0] % 2 !== 0) {
+		arrnew.push(arr[0]);
+	}
+	arrnew = arrnew.concat(collectOddValuesR(arr.slice(1)));
+	return arrnew;
 }
 collectOddValuesR([1, 2, 3, 4, 5]);
 
@@ -109,19 +109,19 @@ collectOddValuesR([1, 2, 3, 4, 5]);
 // power(2,4) // 16
 
 function power(base, exponent) {
-  if (exponent === 0) {
-    return 1;
-  }
-  return base * power(base, exponent - 1);
+	if (exponent === 0) {
+		return 1;
+	}
+	return base * power(base, exponent - 1);
 }
 
 // productOfArray([1,2,3]) // 6
 // productOfArray([1,2,3,10]) // 60
 function productOfArray(arr) {
-  if (arr.length === 0) {
-    return 1;
-  }
-  return arr[0] * productOfArray(arr.slice(1));
+	if (arr.length === 0) {
+		return 1;
+	}
+	return arr[0] * productOfArray(arr.slice(1));
 }
 
 // SAMPLE INPUT/OUTPUT
@@ -129,10 +129,10 @@ function productOfArray(arr) {
 // recursiveRange(10) // 55
 
 function recursiveRange(num) {
-  if (num === 0) {
-    return 0;
-  }
-  return num + recursiveRange(num - 1);
+	if (num === 0) {
+		return 0;
+	}
+	return num + recursiveRange(num - 1);
 }
 
 // fib(4) // 3
@@ -141,9 +141,67 @@ function recursiveRange(num) {
 // fib(35) // 9227465
 
 function fib(num) {
-  // add whatever parameters you deem necessary - good luck!
-  if (num <= 2) {
-    return 1;
-  }
-  return fib(num - 1) + fib(num - 2);
+	// add whatever parameters you deem necessary - good luck!
+	if (num <= 2) {
+		return 1;
+	}
+	return fib(num - 1) + fib(num - 2);
+}
+
+// Print n to 1 using recursion
+function PrintNTo1(n) {
+	if (n < 1) return;
+	console.log(n);
+	PrintNTo1(n - 1);
+}
+
+// print 1 to n using recursion
+let n = 10;
+function Print1ToN(num) {
+	if (num > n) return;
+	console.log(num);
+	Print1ToN(++num);
+}
+
+Print1ToN(1);
+
+//sum of first n natural numbers
+function sumOfNNaturalNumbers(n) {
+	if (n === 0) return 0;
+	return n + sumOfNNaturalNumbers(n - 1);
+}
+
+// sum of all elements in an array
+function sumOfArray(arr) {
+	if (arr.length === 0) return 0;
+	// The idea is add the first element to the sum of the rest of the array
+	//slice creates a shallow copy of the array from index you pass to end
+	return arr[0] + sumOfArray(arr.slice(1));
+}
+sumOfArray([1, 2, 3, 4, 5]);
+
+// sum of all odd numbers in a array
+
+function sumOfOddNumbers(arr) {
+	if (arr.length === 1) return arr[0];
+	if (arr[0] % 2 !== 0) {
+		return arr[0] + sumOfOddNumbers(arr.slice(1));
+	}
+
+	return sumOfOddNumbers(arr.slice(1));
+}
+
+console.log(sumOfOddNumbers([1, 2, 3, 4, 5]));
+
+/*
+1 + sumOfOddNumbers([2, 3, 4, 5])
+1 + sumOfOddNumbers([2,4,5])
+1+ 3 +sumOfOddNumbers([4,5]
+1+3+ sumOfOddNumbers([5])
+*/
+
+//Factorial of N numbers
+function factorialofN(n) {
+	if (n === 0) return 1;
+	return n * factorialofN(n - 1);
 }
--- Searching Alog/LinearSearch.js
+++ Searching Alog/LinearSearch.js
@@ -18,29 +18,37 @@ worst case - O(n)
 */
 
 function linearSearch(arr, num) {
-  let index = 0;
-  if (arr.length === 0) {
-    return -1;
-  }
-  //using for of loop
-  for (let val of arr) {
-    if (val === num) {
-      return index;
-    }
-    index++;
-  }
-  return -1;
+	let index = 0;
+	if (arr.length === 0) {
+		return -1;
+	}
+	//using for of loop
+	for (let val of arr) {
+		if (val === num) {
+			return index;
+		}
+		index++;
+	}
+	return -1;
 }
 console.log(linearSearch([1, 2, 3, 4, 5], 5));
 
 //using for in loop
 function linearSearch(arr, num) {
-  // add whatever parameters you deem necessary - good luck!
-  if (arr.length === 0) {
-    return -1;
-  }
-  for (let key in arr) {
-    if (arr[key] === num) return parseInt(key);
-  }
-  return -1;
+	if (arr.length === 0) {
+		return -1;
+	}
+	for (let key in arr) {
+		if (arr[key] === num) return parseInt(key);
+	}
+	return -1;
 }
+
+function linearSearch(arr, n) {
+	if (arr.length === 0) return -1;
+	for (let i = 0; i <= arr.length; i++) {
+		if (arr[i] === n) return i + 1;
+	}
+	return -1;
+}
+console.log(linearSearch([1, 2, 3, 4, 5, 0, 6, 7], 0));
