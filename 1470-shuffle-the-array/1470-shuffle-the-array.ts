23835cdc31530079a32d8f197ae3612a64444d64
saiteja.kola <saiteja.kola@carrier.com>
2026-05-06 19:07:49 +0530

    Shuffle Array

--- 1470-shuffle-the-array/1470-shuffle-the-array.ts
+++ 1470-shuffle-the-array/1470-shuffle-the-array.ts
@@ -1,8 +1,28 @@
+//shuffle array
+//Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+//Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+function shuffleArray(arr: number[], n: number): number[] {
+	// create a new empty array to hold the shuffled elements
+	// lets start with postions already filled which is arr[0], arr[n+1]
+	// then arr[1], arr[n+2] and so on
+	let result: number[] = [];
+
+	for (let i = 0; i < n; i++) {
+		result.push(arr[i]);
+		result.push(arr[i + n]);
+	}
+	return result;
+}
+
 function shuffle(nums: number[], n: number): number[] {
-    let result:number[] = [];
-    for(let i=0;i<n;i++){
-        result.push(nums[i]);
-        result.push(nums[n+i]);
-    }
-    return result;
-};
+	let result: number[] = new Array(2 * n);
+	for (let i = 0; i < n; i++) {
+		result[2 * i] = nums[i];
+		result[2 * i + 1] = nums[n + i];
+	}
+	return result;
+}
+
+console.log('------------shuffleArray');
+// console.log(shuffleArray([2,5,1,3,4,7],3));
+console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
